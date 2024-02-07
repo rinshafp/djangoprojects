bf64ccafb5a8c2abd7edf6c203d6f6864d462bc4
rinshafp <rinshafp17@gmail.com>
2024-02-07 12:23:18 +0530

    added1

--- ecommerce/cart/admin.py
+++ ecommerce/cart/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from cart.models import Cart,Order,Account
+from django.http import HttpResponse
 admin.site.register(Cart)
 admin.site.register(Order)
 admin.site.register(Account)

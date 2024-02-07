45c597c16c592b50eb994d0e9b1b140209628ac5
rinshafp <rinshafp17@gmail.com>
2024-02-07 11:24:50 +0530

    added

---
+++ ecommerce/search/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from shop.models import Product
+from django.db.models import Q
+
+def search(request):
+    query=""
+    products=None
+    if (request.method=="POST"):
+        query=request.POST['q']
+        if (query):
+            products = Product.objects.filter(Q(name__icontains=query) | Q(desc__icontains=query))
+
+    return render(request, 'search.html', {'query':query,'p':products})
